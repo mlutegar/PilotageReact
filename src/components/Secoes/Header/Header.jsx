7b715128487fd2b44daee9a9d656b5e5606ffced
Michel Lutegar <mlutegar@gmail.com>
2025-04-04 14:33:03 -0300

    feat: adicionando href

--- src/components/Secoes/Header/Header.jsx
+++ src/components/Secoes/Header/Header.jsx
@@ -1,37 +1,62 @@
-import {Link, useNavigate} from "react-router-dom";
-import {Top, Menu, BotaoMenu} from "./Style";
-import React from "react";
+import React, { useContext } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Top, Menu, BotaoMenu } from "./Style";
+import {SecoesContext} from "../../../pages/Home";
 
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
+    // Acessando as refs através do contexto
+    const { sobreNosRef, contatoRef } = useContext(SecoesContext);
 
     const handleLogoClick = () => {
         navigate("/");
-    }
+    };
+
+    const scrollToSection = (ref) => {
+        if (isHomePage && ref && ref.current) {
+            ref.current.scrollIntoView({ behavior: "smooth" });
+        } else {
+            // Se não estiver na página inicial, primeiro navegue para Home e então role para a seção
+            navigate("/");
+            // Precisamos esperar a navegação ser concluída antes de tentar rolar
+            setTimeout(() => {
+                if (ref && ref.current) {
+                    ref.current.scrollIntoView({ behavior: "smooth" });
+                }
+            }, 100);
+        }
+    };
 
     return (
         <>
             <Top>
                 <div className="logo-header" onClick={handleLogoClick}>
-                    <img src="imagens/pilotage.jpg" alt="Logo"/>
+                    <img src="imagens/pilotage.jpg" alt="Logo" />
                 </div>
                 <Menu>
                     <BotaoMenu>
                         <Link to="/">Home</Link>
                     </BotaoMenu>
                     <BotaoMenu>
-                        <Link to='/relatorio-mensal'>Relatórios</Link>
+                        <Link to="/relatorio-mensal">Relatórios</Link>
                     </BotaoMenu>
                     <BotaoMenu>
-                        <a href="#sobre-nos">Sobre Nós</a>
+                        <a onClick={(e) => {e.preventDefault(); scrollToSection(sobreNosRef);}}>
+                            Sobre Nós
+                        </a>
                     </BotaoMenu>
                     <BotaoMenu>
-                        <a href="#contato">Contato</a>
+                        <a onClick={(e) => {e.preventDefault(); scrollToSection(contatoRef);}}>
+                            Contato
+                        </a>
                     </BotaoMenu>
                 </Menu>
             </Top>
         </>
-    )
-}
+    );
+};
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import Base from "./Base"
+import React, { useRef } from "react";
+import Base from "./Base";
 import Banner from "../components/Secoes/Banner/Banner";
 import SobreNos from "../components/Secoes/SobreNos/SobreNos";
 import NossosValores from "../components/Secoes/NossosValores/NossosValores";
@@ -8,23 +9,54 @@ import NossaEquipe from "../components/Secoes/NossaEquipe/NossaEquipe";
 import RelatorioMensalSecao from "../components/Secoes/RelatorioMensalSecao/RelatorioMensalSecao";
 import Contato from "../components/Secoes/Contato/Contato";
 
-const Home = () => {
-  return (
-    <Base>
-        <Banner/>
-        <div id="sobre-nos">
-            <SobreNos/>
-            <NossosValores/>
-        </div>
-        <GestaoRecursos/>
-        <ComoInvestir/>
-        <NossaEquipe/>
-        <RelatorioMensalSecao/>
-        <div id="contato">
-            <Contato/>
-        </div>
-    </Base>
-  )
-}
+// Criando um contexto para compartilhar as refs com outros componentes
+export const SecoesContext = React.createContext({});
 
-export default Home
+const Home = () => {
+    // Criando as refs para cada seção que desejamos referenciar
+    const sobreNosRef = useRef(null);
+    const gestaoRecursosRef = useRef(null);
+    const comoInvestirRef = useRef(null);
+    const nossaEquipeRef = useRef(null);
+    const relatorioRef = useRef(null);
+    const contatoRef = useRef(null);
+
+    // Objeto com todas as refs para compartilhar via contexto
+    const secoes = {
+        sobreNosRef,
+        gestaoRecursosRef,
+        comoInvestirRef,
+        nossaEquipeRef,
+        relatorioRef,
+        contatoRef
+    };
+
+    return (
+        <SecoesContext.Provider value={secoes}>
+            <Base>
+                <Banner />
+                <div ref={sobreNosRef}>
+                    <SobreNos />
+                    <NossosValores />
+                </div>
+                <div ref={gestaoRecursosRef}>
+                    <GestaoRecursos />
+                </div>
+                <div ref={comoInvestirRef}>
+                    <ComoInvestir />
+                </div>
+                <div ref={nossaEquipeRef}>
+                    <NossaEquipe />
+                </div>
+                <div ref={relatorioRef}>
+                    <RelatorioMensalSecao />
+                </div>
+                <div ref={contatoRef}>
+                    <Contato />
+                </div>
+            </Base>
+        </SecoesContext.Provider>
+    );
+};
+
+export default Home;
